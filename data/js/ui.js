5a872937db4f713183538849197886e0ccdcd124
Giovanni Totaro <vanni.totaro@gmail.com>
2013-06-11 00:58:34 +0200

    Show followers breakdown

--- data/js/ui.js
+++ data/js/ui.js
@@ -15,8 +15,18 @@ self.port.on('totalProfiles', function (totalProfiles) {
 
 self.port.on('profile', function (profile) {
   scopeApply(function (scope) {
-    var i = _.sortedIndex(scope.sortedProfiles, profile, function (p) { return -p.followers; });
+    var threshold,
+        j,
+        pf = profile.followers,
+        i = _.sortedIndex(scope.sortedProfiles, profile,
+                          function (p) {return -p.followers;});
     scope.sortedProfiles.splice(i, 0, profile);
+    for (j = 0; j < scope.breakdownThresholds.length; j++) {
+      if (pf >= scope.breakdownThresholds[j]) {
+        scope.breakdown[j]++;
+        break;
+      }
+    }
     scope.timeLatest = new Date();
   });
 });
@@ -29,6 +39,22 @@ function gpfsCtrl($scope) {
   $scope.pageSize = 20;
   $scope.sortedProfiles = [];
   $scope.totalProfiles = 0;
+  $scope.breakdownLabels = [
+    'at least 1,000,000',
+    '100,000 ÷ 999,999',
+    '10,000 ÷ 99,999',
+    '1,000 ÷ 9,999',
+    '100 ÷ 999',
+    '10 ÷ 99',
+    '1 ÷ 9',
+    'no',
+    'n/a'
+  ]
+  $scope.breakdownThresholds = [1000000, 100000, 10000, 1000, 100, 10, 1, 0, -1];
+  $scope.breakdown = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  $scope.breakdownPerc = function (i) {
+    return ($scope.breakdown[i] * 100 / $scope.sortedProfiles.length).toFixed(2) + "%";
+  }
   $scope.numOfPages = function () {
     return Math.ceil($scope.sortedProfiles.length/$scope.pageSize) || 1;
   };
